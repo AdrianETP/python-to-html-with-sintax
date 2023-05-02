aa5591bfe7bbd6756340c96767856c064e64cba0
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-05-01 18:00:40 -0600

    generation of html file

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import re
+import webbrowser
+import time
 
 code = """
 def main():
     x = 2
     print(x)
 """
+
+
+def py_to_html(value, kind):
+    html_content = f"<a class = \"{kind}\" > {value} </a> \n"
+    return html_content
+
+
+html_file = open("index.html", "w")
+
+html_file.write("<link rel=" + "stylesheet" +
+                " href=" + "\"style.css\"" + "/>")
+
+html_file.write("<p>")
 
 
 def tokenize(text):
@@ -23,24 +38,45 @@ def tokenize(text):
         ('STRING', r'"[^"]*"|\'[^\']*\''),
         ('BOOL', r'True|False'),
         ('FLOAT', r'\d+\.\d+'),
-        ('EQUALITY', r'='),
+        ('EQUALITY', r'[=]'),
         ('INEQUALITY', r'[!=]|[<=]|[>=]|[<]|[>]'),
         ('OPERATOR', r'[+]|[-]|[*]|[/]'),
         ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z_0-9]*'),
-        ('PARENTHESIS', r'([(]|[)])',)
+        ('PARENTHESIS', r'([(]|[)])',),
+        ('BRACKETS', r'[{]|[}]|[\[][\]]'),
+        ('NEWLINE', r'\n'),
+        ('WHITESPACE', r'\s'),
     ]
+    x = True
+    # regex de busqueda con identificadores de cada regex
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_especifications)
     print("kind, value , Start, End")
     print("---------------------------------")
+    # iteracion de cada palabra para encontrar que tipo de palabra es
     for mo in re.finditer(tok_regex, text):
+        # el tipo de la palabra
         kind = mo.lastgroup
+        # el valor de la palabra
         value = mo.group()
+        # donde inicia de todo el texto
         start = mo.start()
+        # donde termina
         end = mo.end()
         if (kind == "IDENTIFIER" and value in keywords):
             print("RESERVED", value, start, end)
+            html_content = py_to_html(value, "RESERVED")
+            html_file.write(html_content)
+        elif (kind == "NEWLINE"):
+            print("NEWLINE", value, start, end)
+            html_file.write("<br>")
+        elif (kind == "WHITESPACE"):
+            print("WHITESPACE", value, start, end)
+            html_file.write("&nbsp;")
         else:
             print(kind, value, start, end)
+            htmls_content = py_to_html(value, kind)
+            html_file.write(htmls_content)
 
 
 tokenize(code)
+html_file.write("</p>")
